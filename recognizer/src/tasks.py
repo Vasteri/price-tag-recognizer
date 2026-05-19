fc1aac5ef74a1a3b1bbc094c869abfbcd6b6e7f7
mihalko711 <mihchirkov711@gmail.com>
2026-05-19 12:46:01 +0300

    feat: added choosing best crops + progress bar on ocr

--- recognizer/src/tasks.py
+++ recognizer/src/tasks.py
@@ -11,35 +11,36 @@ def process_video(self, video_path_str: str):
     video_path = Path(video_path_str)
     self.update_state(state="PROCESSING", meta={"progress": 0})
 
-    # пайплайн
-
-    # 1. Отслеживание треков :50% прогресс-бара
-
-    # dir/vid.mp4 -> dir/vid.mp4.tracks/
+    # 1. Трекинг: 0% → 50%
     tracks_path = video_path.parent / (video_path.name + ".tracks")
     tracks_path.mkdir(parents=True, exist_ok=True)
 
     for frames_processed, total_frames in process_tracking(
         source_path=video_path,
         output_path=tracks_path,
-        # пока хардкод + модель с hugging face хорошо себя показывает
         frame_interval=2,
         repo_id="openfoodfacts/price-tag-detection",
         repo_filename="weights/best.pt",
     ):
         self.update_state(
-            state="PROCESSING", meta={"progress": 50 * frames_processed / total_frames}
+            state="PROCESSING",
+            meta={"progress": 50 * frames_processed / total_frames},
         )
 
-    # 2.
+    # 2. Распознавание: 50% → 100%
     self.update_state(state="PROCESSING", meta={"progress": 50})
-    results = recognize_tracks(tracks_path)
+
+    results = []
+    for current, total, batch in recognize_tracks(tracks_path):
+        results.extend(batch)
+        self.update_state(
+            state="PROCESSING",
+            meta={"progress": 50 + 50 * current / total},
+        )
 
     csv_path = Path(
         video_path_str.replace(".mp4", ".csv").replace(UPLOAD_DIR, RESULT_DIR)
     )
     save_to_csv(results, csv_path, video_path.name, tracks_path)
-
-    self.update_state(state="PROCESSING", meta={"progress": 100})
 
     return {"csv_path": str(csv_path)}
